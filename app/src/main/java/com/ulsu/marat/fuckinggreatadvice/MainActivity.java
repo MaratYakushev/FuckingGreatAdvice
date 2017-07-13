7643667dff616f43fcf410e86b7431363dde2ec2
freeman1396 <yakushev1396@gmail.com>
2017-07-13 21:26:31 +0300

    Add Libs

--- app/src/main/java/com/ulsu/marat/fuckinggreatadvice/MainActivity.java
+++ app/src/main/java/com/ulsu/marat/fuckinggreatadvice/MainActivity.java
@@ -3,11 +3,12 @@ package com.ulsu.marat.fuckinggreatadvice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
