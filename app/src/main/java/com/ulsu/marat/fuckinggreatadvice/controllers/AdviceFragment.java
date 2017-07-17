47f4a5bec2f3eb6a3fc9fb189174ff6cd3430b15
freeman1396 <yakushev1396@gmail.com>
2017-07-17 22:43:56 +0300

    Добавил БД для хранения советов

--- app/src/main/java/com/ulsu/marat/fuckinggreatadvice/controllers/AdviceFragment.java
+++ app/src/main/java/com/ulsu/marat/fuckinggreatadvice/controllers/AdviceFragment.java
@@ -5,10 +5,14 @@ import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ulsu.marat.fuckinggreatadvice.R;
+import com.ulsu.marat.fuckinggreatadvice.db.DBHelper;
+import com.ulsu.marat.fuckinggreatadvice.db.DatabaseDao;
+import com.ulsu.marat.fuckinggreatadvice.db.modelSpec.AdviceModel;
 import com.ulsu.marat.fuckinggreatadvice.model.FAdvice;
 import com.ulsu.marat.fuckinggreatadvice.net.GetAdvice;
 import com.ulsu.marat.fuckinggreatadvice.utils.Const;
@@ -16,9 +20,11 @@ import com.ulsu.marat.fuckinggreatadvice.utils.Const;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.InstanceState;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,17 +38,34 @@ public class AdviceFragment extends Fragment {
     @ViewById(R.id.advice_text)
     TextView mAdviceText;
 
+    @Click(R.id.add_button)
+    public void AddData(){
+        addToDB(mFAdvice);
+    }
+
     @Click(R.id.refresh_button)
-    public void refreshClick(){
+    public void refreshClick() {
         Request();
     }
 
-    @AfterViews
-    public void bind(){
-        mAdviceText.setMovementMethod(new ScrollingMovementMethod());
+    @Click(R.id.card_title)
+    public void click(){
+        ReqFromDb();
     }
 
-    private void Request(){
+    @InstanceState
+    String text;
+
+    private FAdvice mFAdvice;
+    private DatabaseDao db;
+
+    @AfterViews
+    public void bind() {
+        mAdviceText.setMovementMethod(new ScrollingMovementMethod());
+        db = DatabaseDao.getDBInstance(getActivity().getApplicationContext());
+    }
+
+    private void Request() {
         Gson gson = new GsonBuilder().create();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Const.RANDOM_ADVICE_URL)
@@ -56,18 +79,34 @@ public class AdviceFragment extends Fragment {
             @Override
             public void onResponse(Call<FAdvice> call, Response<FAdvice> response) {
                 Log.d("TAG", response.body().getText());
-                try {
-                    String res = new String(response.body().getText().getBytes("UTF-8"), "UTF-8");
-                    mAdviceText.setText(Html.fromHtml(res));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
+                String textFix = Html.fromHtml(response.body().getText()).toString();
+                mFAdvice = response.body();
+                mFAdvice.setText(textFix);
+                mAdviceText.setText(mFAdvice.getText());
             }
 
             @Override
             public void onFailure(Call<FAdvice> call, Throwable t) {
-                Log.d("TAG",t.toString());
+                Log.d("TAG", t.toString());
             }
         });
+    }
+
+    private void addToDB(FAdvice fAdvice){
+        int flag = DBHelper.addAdviceToDB(db,fAdvice);
+        if (flag == -1) {
+            PrintToast("Уже есть");
+        }
+    }
+
+    private void ReqFromDb(){
+        List<FAdvice> allAdvices = DBHelper.getAllAdvices(db);
+        for (FAdvice advice : allAdvices) {
+               Log.d("DB", advice.getId() + " " + advice.getText() + "\n");
+        }
+    }
+
+    private void PrintToast(String message){
+        Toast.makeText(getActivity(),message,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/ulsu/marat/fuckinggreatadvice/db/DBHelper.java
@@ -0,0 +1,45 @@
+package com.ulsu.marat.fuckinggreatadvice.db;
+
+import com.ulsu.marat.fuckinggreatadvice.db.modelSpec.AdviceModel;
+import com.ulsu.marat.fuckinggreatadvice.model.FAdvice;
+import com.yahoo.squidb.data.SquidCursor;
+import com.yahoo.squidb.sql.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper {
+
+    public static List<FAdvice> getAllAdvices(DatabaseDao db) {
+        List<FAdvice> adviceList = new ArrayList<>();
+        Query query = Query.select().from(AdviceModel.TABLE);
+        SquidCursor<AdviceModel> adviceCursor = db.query(AdviceModel.class, query);
+        try {
+            AdviceModel adviceModel = new AdviceModel();
+            while (adviceCursor.moveToNext()) {
+                adviceModel.readPropertiesFromCursor(adviceCursor);
+                FAdvice advice = new FAdvice();
+                advice.setId(adviceModel.getAdviceId().toString());
+                advice.setText(adviceModel.getText());
+                advice.setSound(adviceModel.getSound());
+                adviceList.add(advice);
+            }
+        } finally {
+            adviceCursor.close();
+        }
+        return adviceList;
+    }
+
+    public static int addAdviceToDB(DatabaseDao db, FAdvice fAdvice) {
+        AdviceModel adviceModel = new AdviceModel();
+        int count = db.query(AdviceModel.class, Query.select().from(AdviceModel.TABLE).
+                where(AdviceModel.ADVICE_ID.eq(Integer.parseInt(fAdvice.getId())))).getCount();
+        if (count > 0) {
+            return -1;
+        }
+        adviceModel.setAdviceId(Integer.parseInt(fAdvice.getId()));
+        adviceModel.setText(fAdvice.getText());
+        adviceModel.setSound(fAdvice.getSound());
+        return db.persist(adviceModel) ? 1 : 0;
+    }
+}
